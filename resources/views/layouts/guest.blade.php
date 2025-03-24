175febf698087650a66013ae4ad462f11eb6e1fc
danielmordi <danielmordi.codes@gmail.com>
2025-03-24 08:13:56 +0100

    Setup flowbite

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -26,5 +26,8 @@
                 {{ $slot }}
             </div>
         </div>
+
+        <!-- Flowbite JS -->
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
     </body>
 </html>
